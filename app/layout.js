818284fe61bb52cd766ec8e14703afdf66e77b7f
Aaron Lim <aaronlimchengkiat@gmail.com>
2023-06-27 09:40:14 +0800

    updated metadata

--- app/layout.js
+++ app/layout.js
@@ -7,19 +7,26 @@ const inter = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Hyremi",
-  description: "Build by and for freelancers. Connect, showcase, and thrive in our vibrant community.",
+  description:
+    "Build by and for freelancers. Connect, showcase, and thrive in our vibrant community.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="antialiased select-none">
       <Head>
-        <meta property="og:title" content={metadata.title}/>
-        <meta property="og:description" content={metadata.description}/>
-        <meta property="og:image" content="<generated>" />
-        <meta property="og:image:type" content="<generated>" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        <meta property="og:title" content={metadata.title}></meta>
+        <meta property="og:description" content={metadata.description}></meta>
+        <meta property="og:image" content="/opengraph.png"></meta>
+        <meta property="og:image:width" content="1200"></meta>
+        <meta property="og:image:height" content="630"></meta>
+        <meta property="twitter:image" content="/opengraph.png"></meta>
+        <meta property="twitter:card" content={metadata.description}></meta>
+        <meta property="twitter:title" content={metadata.title}></meta>
+        <meta
+          property="twitter:description"
+          content={metadata.description}
+        ></meta>
       </Head>
       <body
         className={`${inter.className}`}
