746480703709c317f63ed9e812f95ee50efbf713
Aaron Lim <aaronlimchengkiat@gmail.com>
2023-07-19 17:51:34 +0800

    install GA4 tracking

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { TailwindIndicator } from "@/components/TailwindIndicator";
+import Script from "next/script";
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,6 +39,18 @@ export default function RootLayout({ children }) {
       >
         {children}
         <TailwindIndicator />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_MEASUREMENT_ID}');
+  `}
+        </Script>
       </body>
     </html>
   );
